d94cdf57f9c8a535e4c8d31d16eab71146858b80
sfalexrog <sfalexrog@gmail.com>
2015-11-20 15:42:15 +0300

    Add a LodePNG-based image loader to preserve my sanity
    Allegro-based image loader doesn't like multi-threaded image loading - at
    least not from threads that are not created by Allegro itself. It offloads
    loading some of the formats to the system, which is generally good, since
    it doesn't require an additional image-loading library, but in case of
    Android and multithreading this causes a load in a thread that might not
    have its JNIEnv pointer - and thus fails.
    
    tl;dr: don't trust Allegro's image loader.

--- framework/imageloader/lodepng_image.cpp
+++ framework/imageloader/lodepng_image.cpp
@@ -2,3 +2,79 @@
 // Created by Alexey on 20.11.2015.
 //
 
+#include "library/sp.h"
+#include "framework/imageloader_interface.h"
+#include "framework/logger.h"
+#include "library/vec.h"
+
+#include <vector>
+#include <physfs.h>
+#include "framework/imageloader/lodepng.h"
+
+using namespace OpenApoc;
+
+namespace
+{
+
+class LodepngImageLoader: public OpenApoc::ImageLoader {
+private:
+
+public:
+    LodepngImageLoader()
+    {
+        // empty?
+    }
+
+    virtual sp<OpenApoc::Image> loadImage(UString path) override
+    {
+        PHYSFS_file *source = PHYSFS_openRead(path.c_str());
+        std::vector<unsigned char> buffer(PHYSFS_fileLength(source));
+        PHYSFS_read(source, &buffer[0], 1, PHYSFS_fileLength(source));
+        PHYSFS_close(source);
+
+        std::vector<unsigned char> image;
+        unsigned int width, height;
+        unsigned int error = lodepng::decode(image, width, height, buffer);
+        if (error)
+        {
+            LogWarning("LodePNG error code: %d: %s", error, lodepng_error_text(error));
+        }
+        if (!image.size())
+        {
+            LogInfo("Failed to load image %s (not a PNG?)", path.c_str());
+            return nullptr;
+        }
+        OpenApoc::Vec2<int> size(width, height);
+        auto img = std::make_shared<OpenApoc::RGBImage>(size);
+        OpenApoc::RGBImageLock dst(img, OpenApoc::ImageLockUse::Write);
+
+        for (int y = 0; y < size.y; y++)
+        {
+            for (int x = 0; x < size.x; x++)
+            {
+                OpenApoc::Colour c(image[4 * size.x * y + 4 * x + 0],
+                                   image[4 * size.x * y + 4 * x + 1],
+                                   image[4 * size.x * y + 4 * x + 2],
+                                   image[4 * size.x * y + 4 * x + 3]);
+                dst.set(OpenApoc::Vec2<int>(x, y), c);
+            }
+        }
+
+        return img;
+    }
+
+    virtual UString getName() override { return "lodepng"; }
+};
+
+class LodepngImageLoaderFactory : public OpenApoc::ImageLoaderFactory
+{
+public:
+    virtual OpenApoc::ImageLoader *create() override { return new LodepngImageLoader(); }
+    virtual ~LodepngImageLoaderFactory() {}
+};
+
+    OpenApoc::ImageLoaderRegister<LodepngImageLoaderFactory> register_at_load_lodepng_image("lodepng");
+
+}
+
+
